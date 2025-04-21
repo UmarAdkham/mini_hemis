6fa65c1faca91b3c7c35273690dec3a96d5522f4
islome <islombekkamoliddinoff05@gmail.com>
2025-04-21 20:31:01 +0500

    filepath improvment

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const studentRouter = express.Router();
-const { getStudenttask } = require('../controllers/student/viewGrade');
+const { getStudenttask, getFilepath, upload } = require('../controllers/student/viewGrade');
 studentRouter.get('/:student_id', getStudenttask); // student_id bo'yicha ma'lumotlarni olish
+studentRouter.post('/upload',upload, getFilepath); // Fayl yuklash
 module.exports = studentRouter;
