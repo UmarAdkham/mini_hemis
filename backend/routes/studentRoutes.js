1390c37892491d8c404f7aad7871eac2298c30a8
rahmonovsaydullo <saydullo779@gmail.com>
2025-04-21 23:14:07 +0500

    I created submit task API. And changed student routes

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const createStudent = require('../controllers/admin/createStudent');
+const submitTask = require('../controllers/student/submitWork');
 
-const router = express.Router();
+const studentRouter = express.Router();
 
-router.post('/', createStudent);
+studentRouter.post('/', createStudent);
+studentRouter.post('/task/:course_id', submitTask);
 
-module.exports = router;
+module.exports = studentRouter;
