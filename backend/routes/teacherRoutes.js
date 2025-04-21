f52a8bca2235a39b177b83acf7db462391781473
Ibrohimov-Zafar <ibragimovzafar001@gmail.com>
2025-04-21 20:17:19 +0500

    xato tugatildi

--- backend/routes/teacherRoutes.js
+++ backend/routes/teacherRoutes.js
@@ -2,6 +2,4 @@ const express = require('express');
 const router = express.Router();
 const gradeWork = require('../controllers/teacher/gradeStudent');
 router.put('/:id/grade',gradeWork);
-
-
 module.exports =  router
