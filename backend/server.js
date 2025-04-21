a04749a67269efb74f182f7a519da95223094ea1
shokhruzbek <ashokhruzbek@gmail.com>
2025-04-21 19:27:06 +0500

    i added authRoutes

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,19 @@ const app = express();
 
 const studentRoutes = require('./routes/studentRoutes');
 const teacherRoutes = require('./routes/teacherRoutes');
+const authRoutes = require('./routes/authRoutes');
+
 
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
-// app.use('/', authRoutes);
+app.use('/', authRoutes);
 // app.use('/admin', adminRoutes);
 // app.use('/teacher', teacherRoutes);
 app.use('/student', studentRoutes);
-app.use('/teacher', teacherRoutes)
+app.use('/teacher', teacherRouutes);
 
 app.listen(PORT, () => {
   console.log(`Girgitton ${PORT}-portda xizmatda`);
