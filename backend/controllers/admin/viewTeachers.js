6083b5dc2d177a663e44341909c965d0845d371d
umidjon <161438407+umid03@users.noreply.github.com>
2025-04-22 12:34:08 +0500

    yangilangani

--- backend/controllers/admin/viewTeachers.js
+++ backend/controllers/admin/viewTeachers.js
@@ -1,10 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: false },
-});
+
 
 const getAllTeachers = async (req, res) => {
   try {
