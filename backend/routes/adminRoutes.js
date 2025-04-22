a68c39f3becdb9c0499003b04604164736d1de22
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2025-04-22 12:38:30 +0500

    2

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,11 +5,13 @@ const { createCourse } = require("../controllers/admin/createCourse");
 const { deleteTeacher } = require("../controllers/admin/deleteTeacher");
 const { deleteStudent } = require("../controllers/admin/deleteStudent");
 const getAllTeachers = require("../controllers/admin/viewTeachers");
+const viewStudents = require("../controllers/admin/viewStudents");
 
 const adminRoute = express.Router();
 
-adminRoute.post("/add-teacher", createTeacher);
 adminRoute.get("/teachers", getAllTeachers);
+adminRoute.get("/students", viewStudents);
+adminRoute.post("/add-teacher", createTeacher);
 adminRoute.post("/add-student", createStudent);
 adminRoute.delete("/delete-teacher/:id", deleteTeacher);
 adminRoute.delete("/delete-student/:id", deleteStudent);
