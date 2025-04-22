197c12b9f037ab61dafcc95022f3c378feb94b2d
Fazliddin <mrfazli1221@gmail.com>
2025-04-22 12:40:53 +0500

    изменений в удалений из базы в удалений с курса
    поменял функцию в сторону изменении в части удаления студента из курса

--- backend/controllers/admin/deleteTeacher.js
+++ backend/controllers/admin/deleteTeacher.js
@@ -1,32 +1,32 @@
-const pool = require('../db');
+const pool = require('../../config/db');
 
-const deleteStudent = async (req, res) => {
+const removeStudentFromCourse = async (req, res) => {
   const teacherId = req.user.id;
-  const studentId = parseInt(req.params.id);
+  const { studentId, courseId } = req.body;
 
   try {
     const result = await pool.query(
-      `
-      SELECT 1
-      FROM users u
-      JOIN enrollment e ON u.id = e.student_id
-      JOIN courses c ON c.id = e.course_id
-      WHERE u.id = $1 AND u.role = 'student' AND c.teacher_id = $2
-      LIMIT 1
-      `,
-      [studentId, teacherId]
+      
+      `SELECT 1
+      FROM courses
+      WHERE id = $1 AND teacher_id = $2`
+      ,
+      [courseId, teacherId]
     );
 
     if (result.rowCount === 0) {
-      return res.status(403).json({ message: 'Access denied or student not found' });
+      return res.status(403).json({ message: 'Access denied or course not found' });
     }
 
-    await pool.query(`DELETE FROM users WHERE id = $1`, [studentId]);
+    await pool.query(
+      `DELETE FROM enrollment WHERE student_id = $1 AND course_id = $2`,
+      [studentId, courseId]
+    );
 
-    res.status(200).json({ message: 'Student deleted successfully' });
+    res.status(200).json({ message: 'Student removed from course' });
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
   }
 };
 
-module.exports = { deleteStudent };
+module.exports = { removeStudentFromCourse };
