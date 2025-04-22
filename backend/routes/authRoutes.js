a40a09864d2c7f337776d5d2d1d7e8f19b4d4286
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2025-04-22 09:45:12 +0500

    feat: edit profile

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { login } = require("../controllers/auth/login");
+const { editProfile } = require("../controllers/auth/editProfile");
 
 const authRoutes = express.Router();
 
 authRoutes.get("/login", login);
+authRoutes.patch("/edit-profile", editProfile);
 
 module.exports = authRoutes;
