b7b3324a79b99e0494f095ae66e0e7bbe2ae76af
rahmonovsaydullo <saydullo779@gmail.com>
2025-04-22 11:52:15 +0500

    I created upload middleware

--- backend/middlewares/uploadFile.js
+++ backend/middlewares/uploadFile.js
@@ -1,28 +1,35 @@
 const multer = require('multer');
 
+const allowedTypes = [
+    'image/png',
+    'image/jpeg',
+    'image/webp',
+    'application/pdf',
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+];
 
-const allowedTypes = ['image/png', 'image/jpeg', 'image/webp'];
 
 const uploadStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '_' + file.originalname);
-  }
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '_' + file.originalname);
+    }
 });
 
 const fileFilter = (req, file, cb) => {
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error('Only PNG, JPEG, and WEBP images are allowed'), false);
-  }
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only PNG, JPEG, WEBP, PDF, and DOC  images are allowed'), false);
+    }
 };
 
 const upload = multer({
-  storage: uploadStorage,
-  fileFilter,
+    storage: uploadStorage,
+    fileFilter,
 });
 
 const uploadMiddleware = upload.single('photo');
