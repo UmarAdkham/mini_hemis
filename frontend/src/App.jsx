ad42488ea9b041e1362c95bd697c6cfe9d609868
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2025-04-23 09:08:56 +0500

    fix: issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import routes from "./routes";
 import NotFound from "./pages/notFound";
 import Login from "./pages/login";
-import StudentWorkPage from "./pages/Teacher/TaskGradePage";
-import AdminPage from "./pages/Admin/AddUserpage";
 function App() {
   return (
     <Routes>
@@ -26,8 +24,6 @@ function App() {
         </Route>
       ))}
       {/* Not found */}
-      <Route path="/gradeWork" element={<StudentWorkPage/>}/>
-      <Route path="/addUser" element={<AdminPage/>}/>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
