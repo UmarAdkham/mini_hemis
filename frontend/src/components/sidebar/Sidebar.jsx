2cf4f10d750ecc0a74a1d6289cc9eff3e5bdf1c6
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2025-04-23 18:23:48 +0500

    fix: sidebardagi kamchiliklar to'g'irlandi

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -46,14 +46,6 @@ function Sidebar({ paths }) {
             </span>
           </a>
           <ul className="space-y-2 font-medium">
-            {/* <li>
-              <Link
-                to={`/teacher`}
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <span className="ms-3">Teacher</span>
-              </Link>
-            </li> */}
             {paths.map((path) => (
               <li key={path}>
                 <NavLink
