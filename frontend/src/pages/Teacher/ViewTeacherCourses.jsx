5bc83ae736c821fde2f5496afb9172fe28555e26
shokhruzbek <ashokhruzbek@gmail.com>
2025-04-23 18:35:59 +0500

    some updates

--- frontend/src/pages/Teacher/ViewTeacherCourses.jsx
+++ frontend/src/pages/Teacher/ViewTeacherCourses.jsx
@@ -43,7 +43,7 @@ function ViewAllCourses() {
     return (
         <div className="max-w-6xl mx-auto px-4 py-10">
             <h2 className="text-3xl font-bold mb-6 text-center text-indigo-700">O'qituvchining Kurslari</h2>
-            <p className="text-center text-gray-600 mb-8 italic">{message}</p>
+            {/* <p className="text-center text-gray-600 mb-8 italic">{message}</p> */}
 
             {courses.length > 0 ? (
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
