4131d2bb2922f0eb92f1f4c3936c6939b1230f3a
islome <islombekkamoliddinoff05@gmail.com>
2025-04-24 10:29:23 +0500

    feat: little fixing

--- backend/controllers/student/viewCourses.js
+++ backend/controllers/student/viewCourses.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-const pool = require(".../config/db");
 
-exports.getAllCourses = async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM courses");
-
-    if (result.rows.length === 0) {
-      return res.status(404).send({ message: "Kurslar topilmadi" });
-    }
-
-    res.status(200).json({
-      message: "Kurslar muvaffaqiyatli olindi",
-      data: result.rows,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Kurslarni olishda xatolik yuz berdi" });
-=======
 const pool = require("../../config/db");
 exports.viewCourseStudents = async (req, res) => {
   try {
@@ -49,6 +31,5 @@ exports.viewCourseStudents = async (req, res) => {
   } catch (error) {
     console.error("Xatolik:", error);
     res.status(500).json({ message: "Talabalarni olishda xatolik yuz berdi" });
->>>>>>> 564b437d82f72cd4731362773fbfe42ff4008aa3
   }
 };
