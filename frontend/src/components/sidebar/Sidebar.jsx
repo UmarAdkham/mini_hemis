02445ef709c02c248aeb9d4e41b1fcefd0a6656d
Tohirjon Yusupov <yusupovtohir12@gmail.com>
2025-04-24 10:57:32 +0500

    fixed

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -69,7 +69,7 @@ function Sidebar({ paths, panelName }) {
                 </NavLink>
                 ) : (
                   <NavLink
-                  to={`/teacher/${path}`}
+                  to={`/${panelName}/${path}`}
                   className={({ isActive }) =>
                     `flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${
                       isActive ? "bg-gray-100 dark:bg-gray-700" : ""
