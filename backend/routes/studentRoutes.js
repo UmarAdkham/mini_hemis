cfe7545964743a878e9f38dc31aa73625f0f6b27
Umar Adkhamov <desakondo697@gmail.com>
2025-04-24 10:40:14 +0500

    ViewAllCourses and ViewStudentCourses

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -8,13 +8,14 @@ const { getAllCourses } = require("../controllers/teacher/viewCourses");
 const { getStudenttask } = require("../controllers/student/viewGrade");
 const submitWork = require("../controllers/student/submitWork");
 const { checkRole } = require("../middlewares/checkRole");
+const { viewCourseStudents } = require("../controllers/student/viewCourses");
 
 studentRoute.use(checkRole(["student"]));
 
 studentRoute.post("/", createStudent);
 studentRoute.post("/submit/:task_id", uploadMiddleware, submitWork);
 studentRoute.get("/view-grades/:student_id", getStudenttask);
-studentRoute.post("/view-courses", getAllCourses);
-
+studentRoute.get("/view-all-courses/", getAllCourses);
+studentRoute.get("/view-my-courses/:student_id", viewCourseStudents);
 
 module.exports = studentRoute;
